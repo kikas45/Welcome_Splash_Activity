d0af79664f770007d28d6959ff049fef6d37eb34
kikas45 <tigera1759@gmail.com>
2022-11-23 10:22:53 +0100

    Welcome_Splash_Activity

---
+++ app/src/main/java/com/example/splashscreendemo/MainActivity.java
@@ -0,0 +1,16 @@
+package com.example.splashscreendemo;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setTheme(R.style.Theme_SplashScreenDemo);
+        setContentView(R.layout.activity_main);
+        getSupportActionBar().hide();
+    }
+}
